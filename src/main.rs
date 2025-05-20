9af5379285d2e02edb7a843c32ca86c07a9ac065
segatcm <segatheycallme@proton.me>
2025-05-20 22:06:07 +0200

    mgelectronic web scraping partly done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use mgelectronic::simple_search;
+use reqwest::Client;
+
+mod mgelectronic;
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+    dbg!(
+        simple_search("SPREJ 100 ML PCB ČISTAČ", &client)
+            .await
+            .unwrap()
+    );
 }
---
+++ src/mgelectronic.rs
@@ -0,0 +1,130 @@
+use std::error::Error;
+
+use reqwest::{Client, Url};
+use scraper::{ElementRef, Selector};
+use thiserror::Error;
+
+const BASE_URL: &str = "https://mgelectronic.rs/search";
+
+#[derive(Debug, Error)]
+pub enum MGError {
+    #[error("Couldn't find main table")]
+    NoTable,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct MGElectronicProduct {
+    name: String,
+    price: f64,
+    stock: bool,
+    product_url: String,
+    image_url: Option<String>,
+    datasheet_url: Option<String>,
+    code: String,
+    characteristics: String,
+    housing: String,
+    manufacturer: Option<String>,
+    manufacturer_code: Option<String>,
+}
+
+// #[allow(dead_code)]
+pub async fn simple_search(
+    search: &str,
+    client: &Client,
+) -> Result<Vec<MGElectronicProduct>, Box<dyn Error>> {
+    let url = Url::parse_with_params(BASE_URL, [("q", search)])?;
+    let body = client.get(url.to_string()).send().await?.text().await?;
+    let document = scraper::html::Html::parse_document(&body);
+    let mut rows = document
+        .select(&Selector::parse(".search-results table tbody").unwrap())
+        .next()
+        .ok_or(MGError::NoTable)?
+        .child_elements();
+    rows.next(); // skip the header row
+
+    Ok(rows
+        .map(|row| {
+            let inner = parse_row(row).unwrap();
+            MGElectronicProduct {
+                product_url: url.join(&inner.product_url).unwrap().to_string(),
+                datasheet_url: inner
+                    .datasheet_url
+                    .map(|str| url.join(&str).unwrap().to_string()),
+                ..inner
+            }
+        })
+        .collect())
+}
+
+fn parse_row(row: ElementRef) -> Option<MGElectronicProduct> {
+    let mut td_iter = row.child_elements();
+    let image_url = td_iter
+        .next()
+        .and_then(|el| {
+            el.select(&Selector::parse("img").unwrap())
+                .next()?
+                .attr("src")
+        })
+        .map(|str| str.to_string());
+
+    let description = td_iter.next()?;
+    let (name, product_url) = description
+        .select(&Selector::parse("h4 a").unwrap())
+        .next()
+        .and_then(|el| {
+            Some((
+                el.inner_html().trim().to_string(),
+                el.attr("href")?.to_string(),
+            ))
+        })?;
+    let dd_selector = Selector::parse(".product-spec-list dd").unwrap();
+    let mut dd_iter = description
+        .select(&dd_selector)
+        .map(|el| el.text().collect::<String>().trim().to_string()); // ugh
+    let code = dd_iter.next()?;
+    let characteristics = dd_iter.next()?;
+    let housing = dd_iter.next()?;
+    let manufacturer = dd_iter.next();
+    let manufacturer_code = dd_iter.next();
+
+    let datasheet_url = td_iter
+        .next()
+        .and_then(|el| {
+            el.select(&Selector::parse("a").unwrap())
+                .next()?
+                .attr("href")
+        })
+        .map(|str| str.to_string());
+
+    let stock = &td_iter
+        .next()?
+        .select(&Selector::parse("span").unwrap())
+        .next()?
+        .inner_html()
+        == "dostupno"; // only works for serbian
+
+    td_iter.next();
+    let price: f64 = td_iter
+        .next()?
+        .select(&Selector::parse("span").unwrap())
+        .next()?
+        .inner_html()
+        .replace(',', ".")
+        .parse()
+        .ok()?;
+
+    Some(MGElectronicProduct {
+        name,
+        manufacturer,
+        manufacturer_code,
+        image_url,
+        product_url,
+        characteristics,
+        code,
+        housing,
+        price,
+        stock,
+        datasheet_url,
+    })
+}
